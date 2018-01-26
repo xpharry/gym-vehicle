afa081e28b0cecdd1b7e9a7099a8e225462199dd
xpharry <robotpengxu@gmail.com>
2018-01-25 19:35:50 -0500

    python launch empty world and catvehicle

---
+++ experiments/catvehicle/catvehicle_qlearn.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import gym
+import gym_vehicle
+import time
+import numpy
+import random
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+import qlearn
+
+
+if __name__ == '__main__':
+
+    env = gym.make('GazeboCatvehicleLidar-v0')
